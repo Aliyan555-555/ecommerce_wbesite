99a44910b438292b536eae5b74026cf4307c3a35
Aliyan <aliyansiddiqui555@gmail.com>
2024-02-06 23:47:49 +0500

    hello

--- ecomm/app/Product/[id]/page.js
+++ ecomm/app/Product/[id]/page.js
@@ -4,24 +4,18 @@ import { client } from '@/lib/client'
 import { useEffect,useState } from 'react'
 import StaticProduct from '@/components/StaticProduct'
 import Head from 'next/head'
+import { GlobleContext } from '@/Context/StateContext'
 function Product ({params}){
-  const [product, setproduct] = useState([]);
-  const getData=async()=>{
-    const qurye=`*[_type == 'Product']`
-    const Productdata= await client.fetch(qurye)
-    const product=await Productdata
-    setproduct(product)
-  }
-  const title = product?.filter((curentElem)=>curentElem._id===params.id).map((elem)=>elem.title)
+  const {Products} = GlobleContext() 
+  const title = Products?.filter((curentElem)=>curentElem._id===params.id).map((elem)=>elem.title)
   useEffect(()=>{
     document.title=title
-    getData()
   })
  
   return (
     <div className='w-full h-[100vh] pt-[10rem]'>
      
-      {product?.filter((curentElem)=>curentElem._id===params.id).map((elem)=>{
+      {Products?.filter((curentElem)=>curentElem._id===params.id).map((elem)=>{
        return <>
         
        <StaticProduct key={elem._id} data={elem}/></>
